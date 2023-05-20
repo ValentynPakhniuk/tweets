68dd13a5b30dc0a592b95d642832df38dc479a25
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 01:38:00 +0300

    CREATE: backend requests

---
+++ src/redux/users/operations.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+axios.defaults.baseURL = "https://64500800b61a9f0c4d31a1ad.mockapi.io/api/v1";
+
+export const fetchUsersThunk = createAsyncThunk(
+  "users/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(`/users`);
+      return response.data;
+    } catch (e) {
+      toast.error(e.message);
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+export const updateUsersFollowersThunk = createAsyncThunk(
+  "users/update",
+  async (user, thunkAPI) => {
+    try {
+      const response = await axios.put(`/users/${user.id}`, { ...user });
+      return response.data;
+    } catch (e) {
+      toast.error(e.message);
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
