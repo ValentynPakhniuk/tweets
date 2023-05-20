ecdd539ade801d82660b391408ba2e9dce9359e5
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 02:20:42 +0300

    DELETED: import React

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import UserItem from "../UserItem/UserItem";
 import { CardList, Container } from "./UsersList.styled";
