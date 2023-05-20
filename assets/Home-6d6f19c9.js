7c74cff375c87487d7dab96ae397df15a5c984a4
ValentynPakhniuk <111594572+ValentynPakhniuk@users.noreply.github.com>
2023-05-20 23:27:08 +0000

    Deploying to gh-pages from @ ValentynPakhniuk/tweets@eacb6a3872a070347c0dabcad48105fc0cc0bd82 🚀

---
+++ assets/Home-6d6f19c9.js
@@ -0,0 +1,1 @@
+import{j as e,C as o}from"./index-dcb63553.js";function r(){return e.jsx(o,{children:e.jsx("h1",{children:"Welcome to the tweet home page. I wish you a good time."})})}export{r as default};
