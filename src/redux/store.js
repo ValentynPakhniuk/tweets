928f1fe87a1b7fd59aae8390be88c3870dff2144
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 01:40:42 +0300

    CREATE: store

---
+++ src/redux/store.js
@@ -0,0 +1,37 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { usersReducer } from "./users/usersSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const usersPersistConfig = {
+  key: "users",
+  storage,
+  whitelist: ["followedUsers"],
+};
+
+export const store = configureStore({
+  reducer: {
+    users: persistReducer(usersPersistConfig, usersReducer),
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
+});
+
+export const persistor = persistStore(store);
