b7029a4bffbe08b1f2b454827ddb6a3526cae2c2
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 01:44:48 +0300

    CREATE: styles header

---
+++ src/components/AppBar/AppBar.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0;
+  margin-bottom: 40px;
+  border-bottom: 1px solid #646cff;
+  & nav {
+    margin-left: 60px;
+  }
+`;
