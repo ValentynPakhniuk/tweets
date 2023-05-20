443fdd0b66740b6c3ccf452b4a3e7889027c8b69
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 01:41:15 +0300

    CREATE: home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Container } from "../components/UsersList/UsersList.styled";
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Welcome to the tweet home page. I wish you a good time.</h1>
+    </Container>
+  );
+}
