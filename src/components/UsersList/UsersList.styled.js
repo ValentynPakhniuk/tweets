668c22d0ec7bf63caa31c60708d19340536a7728
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 01:56:54 +0300

    CREATE: stiles for CardList

---
+++ src/components/UsersList/UsersList.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+`;
+
+export const CardList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  width: 1200px;
+  gap: 30px;
+`;
