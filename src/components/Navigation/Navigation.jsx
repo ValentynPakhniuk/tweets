1ed0001a64dd3603fa57195aad09c01acf0784bc
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 01:46:37 +0300

    CREATE: navigation+location

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { useLocation } from "react-router-dom";
+import { SiteNavLink } from "./Navigation.styled";
+
+export const Navigation = () => {
+  const location = useLocation();
+  return (
+    <nav>
+      <SiteNavLink to="/">Home</SiteNavLink>
+      <SiteNavLink to="/users" state={{ from: location }}>
+        Users
+      </SiteNavLink>
+    </nav>
+  );
+};
