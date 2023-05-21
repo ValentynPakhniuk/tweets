6d548bbc710795ac14e0ef700bac5cd43a6fa75c
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 12:06:28 +0300

    UPDATE: removed the redundant code

--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -1,12 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Helmet } from "react-helmet-async";
-import { fetchUsersThunk } from "../redux/users/operations";
 import { UsersList } from "../components/UsersList/UsersList";
 import { selectError, selectIsLoading } from "../redux/users/selectors";
 
 const Tweets = () => {
-  const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
 
