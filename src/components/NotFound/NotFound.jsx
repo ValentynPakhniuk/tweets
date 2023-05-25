2b46de2f74776f1652a5c255b6a44c2d893904cf
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-26 02:04:24 +0300

    ADDED: title

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -3,15 +3,21 @@ import { TitleNotFound } from "./NotFound.styled";
 import { useRef } from "react";
 import { Button } from "../Button/Button.styled";
 import { Container } from "../UsersList/UsersList.styled";
+import { Helmet } from "react-helmet-async";
 
 export const NotFound = () => {
   const backLinkLocationRef = useRef(location.state?.from ?? "/");
   return (
-    <Container>
-      <TitleNotFound>Not Found 404</TitleNotFound>
-      <Link to={backLinkLocationRef.current}>
-        <Button>Go home</Button>
-      </Link>
-    </Container>
+    <>
+      <Helmet>
+        <title>404</title>
+      </Helmet>
+      <Container>
+        <TitleNotFound>Not Found 404</TitleNotFound>
+        <Link to={backLinkLocationRef.current}>
+          <Button>Go home</Button>
+        </Link>
+      </Container>
+    </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet-async";
 import { Container } from "../components/UsersList/UsersList.styled";
 
 export default function Home() {
   return (
-    <Container>
-      <h1>Welcome to the tweet home page. I wish you a good time.</h1>
-    </Container>
+    <>
+      <Helmet>
+        <title>Tweets</title>
+      </Helmet>
+      <Container>
+        <h1>Welcome to the tweet home page. I wish you a good time.</h1>
+      </Container>
+    </>
   );
 }
